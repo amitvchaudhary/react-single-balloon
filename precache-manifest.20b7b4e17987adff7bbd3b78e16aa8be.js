ac7ecc047db486cca9fc7e5fce6f9bea00925c75
amitvchaudhary <amit.chaudhary140@gmail.com>
2022-06-08 12:15:50 +0530

    Updates

---
+++ precache-manifest.20b7b4e17987adff7bbd3b78e16aa8be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff5798c6c4136aea16f24857686f118",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3afe82aa2f405b842444",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "50673abbd73e32d87e06",
+    "url": "./static/js/2.bd411ded.chunk.js"
+  },
+  {
+    "revision": "46306d030c42396fd95fe1eb6fa67ec8",
+    "url": "./static/js/2.bd411ded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3afe82aa2f405b842444",
+    "url": "./static/js/main.7ec471b7.chunk.js"
+  },
+  {
+    "revision": "59bd054f3bde6b0e219b610549d13f6f",
+    "url": "./static/js/main.7ec471b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71df1403e09f3847f858",
+    "url": "./static/js/runtime-main.4cda660f.js"
+  }
+]);
